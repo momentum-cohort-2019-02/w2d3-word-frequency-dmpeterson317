b276ff12f6aa546c9711eadbd95a77721e0f7052
dmpeterson317 <dmpeterson317@gmail.com>
2019-02-20 21:09:06 -0500

    initial commit

--- word_frequency.py
+++ word_frequency.py
@@ -1,3 +1,5 @@
+import string
+
 STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
@@ -5,10 +7,39 @@ STOP_WORDS = [
 ]
 
 
-def print_word_freq(file):
-    """Read in `file` and print out the frequency of words in that file."""
-    pass
+def normalize_text(text):
+    """given a text, lowercases it, removes all punctuation, and replaces all whitespace with normal spaces. Multiple whitespace will be compressed into a single space"""
+    
+    text = text.casefold()
+    valid_chars = string.ascii_letters + string.whitespace + string.digits
+    new_text = ""
+    for char in text:
+        if char in valid_chars:
+            new_text += char
+        
+    text = new_text
+    text = text.replace("\n", " ")
+    return text
 
+def print_word_freq(filename):
+    """Read in `filename` and print out the frequency of words in that file."""
+    # with statement will auto close the file
+    # i.e. file.close()
+    with open(filename) as file:
+        text = file.read()
+
+    text = normalize_text(text)
+    
+    word_freq = {}
+    for word in text.split(" "):
+        if word != '' and word not in STOP_WORDS:
+            if word in word_freq:
+                word_freq[word] += 1 
+            else:
+                word_freq[word] = 1
+
+    for word, freq in word_freq.items():
+        print(word, "|", freq, "*" * freq)
 
 if __name__ == "__main__":
     import argparse
@@ -20,6 +51,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     file = Path(args.file)
+    # file = Path("seneca_falls.txt")
     if file.is_file():
         print_word_freq(file)
     else:
